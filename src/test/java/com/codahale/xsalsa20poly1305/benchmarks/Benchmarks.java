23fa371f928ff41ae4e56913343a8cde0d8c09af
Coda Hale <coda.hale@gmail.com>
2017-04-23 15:48:19 -0700

    Move benchmarks to test directory.

---
+++ src/test/java/com/codahale/xsalsa20poly1305/benchmarks/Benchmarks.java
@@ -0,0 +1,12 @@
+package com.codahale.xsalsa20poly1305.benchmarks;
+
+import java.io.IOException;
+import org.openjdk.jmh.Main;
+import org.openjdk.jmh.runner.RunnerException;
+
+public class Benchmarks {
+
+  public static void main(String[] args) throws IOException, RunnerException {
+    Main.main(args);
+  }
+}
