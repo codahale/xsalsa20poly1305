021dfc7c36c8ecb6d942a103014d11122590c9c0
Coda Hale <coda.hale@gmail.com>
2017-04-24 15:37:05 -0700

    Add internal HSalsa20 implementation.

---
+++ src/main/java/com/codahale/xsalsa20poly1305/HSalsa20.java
@@ -0,0 +1,59 @@
+package com.codahale.xsalsa20poly1305;
+
+import static org.bouncycastle.util.Pack.intToLittleEndian;
+import static org.bouncycastle.util.Pack.littleEndianToInt;
+
+import java.nio.charset.StandardCharsets;
+import org.bouncycastle.crypto.engines.Salsa20Engine;
+
+/**
+ * An implementation of the HSalsa20 hash based on the Bouncy Castle Salsa20 core.
+ */
+interface HSalsa20 {
+
+  byte[] SIGMA = "expand 32-byte k".getBytes(StandardCharsets.US_ASCII);
+
+  static void hsalsa20(byte[] out, byte[] in, byte[] k, byte[] c) {
+    final int[] x = new int[16];
+
+    x[0] = littleEndianToInt(c, 0);
+    x[1] = littleEndianToInt(k, 0);
+    x[2] = littleEndianToInt(k, 4);
+    x[3] = littleEndianToInt(k, 8);
+    x[4] = littleEndianToInt(k, 12);
+    x[5] = littleEndianToInt(c, 4);
+
+    x[6] = littleEndianToInt(in, 0);
+    x[7] = littleEndianToInt(in, 4);
+    x[8] = littleEndianToInt(in, 8);
+    x[9] = littleEndianToInt(in, 12);
+
+    x[10] = littleEndianToInt(c, 8);
+    x[11] = littleEndianToInt(k, 16);
+    x[12] = littleEndianToInt(k, 20);
+    x[13] = littleEndianToInt(k, 24);
+    x[14] = littleEndianToInt(k, 28);
+    x[15] = littleEndianToInt(c, 12);
+
+    Salsa20Engine.salsaCore(20, x, x);
+
+    x[0] -= littleEndianToInt(c, 0);
+    x[5] -= littleEndianToInt(c, 4);
+    x[10] -= littleEndianToInt(c, 8);
+    x[15] -= littleEndianToInt(c, 12);
+
+    x[6] -= littleEndianToInt(in, 0);
+    x[7] -= littleEndianToInt(in, 4);
+    x[8] -= littleEndianToInt(in, 8);
+    x[9] -= littleEndianToInt(in, 12);
+
+    intToLittleEndian(x[0], out, 0);
+    intToLittleEndian(x[5], out, 4);
+    intToLittleEndian(x[10], out, 8);
+    intToLittleEndian(x[15], out, 12);
+    intToLittleEndian(x[6], out, 16);
+    intToLittleEndian(x[7], out, 20);
+    intToLittleEndian(x[8], out, 24);
+    intToLittleEndian(x[9], out, 28);
+  }
+}
---
+++ src/test/java/com/codahale/xsalsa20poly1305/HSalsa20Test.java
@@ -0,0 +1,24 @@
+package com.codahale.xsalsa20poly1305;
+
+import static com.codahale.xsalsa20poly1305.tests.Generators.byteArrays;
+import static org.quicktheories.quicktheories.QuickTheory.qt;
+
+import com.github.nitram509.jmacaroons.crypto.neilalexander.jnacl.hsalsa20;
+import com.github.nitram509.jmacaroons.crypto.neilalexander.jnacl.xsalsa20;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class HSalsa20Test {
+
+  @Test
+  public void interoperability() throws Exception {
+    qt().forAll(byteArrays(16, 16), byteArrays(32, 32))
+        .check((in, key) -> {
+          final byte[] a = new byte[32];
+          final byte[] b = new byte[32];
+          hsalsa20.crypto_core(a, in, key, xsalsa20.sigma);
+          HSalsa20.hsalsa20(b, in, key, HSalsa20.SIGMA);
+          return Arrays.equals(a, b);
+        });
+  }
+}
